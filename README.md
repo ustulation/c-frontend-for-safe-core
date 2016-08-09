1de2b728c9629a0af9a4893a5e39a178e06d3dcf
ustulation <ustulation@gmail.com>
2016-08-09 13:18:13 +0100

    feat/c-code: make initial commit Write C code to interface with and test safe_core's FFI API.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Linux
+1. Compile [safe_core](https://github.com/maidsafe/safe_core) to get `libsafe_core.so`.
+2. Compile this C code as `gcc -O2 main.c -L/path/to/libsafe_core.so -lsafe_core -o test.out`
+  - e.g. if you copied the shared object to pwd, then do `gcc -O2 main.c -L. -lsafe_core -o test.out`
+3. Run the executable with `LD_LIBRARY_PATH=/path/to/libsafe_core.so ./test.out`
+  - e.g. if you copied the shared object to pwd, then do `LD_LIBRARY_PATH=. ./test.out`
---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+extern int init_logging();
+extern int create_unregistered_client(void**);
+extern int client_issued_gets(void*);
+extern int execute(char*, void*);
+extern char* execute_for_content(char*, int*, int*, int*, void*);
+
+int main() {
+    int res = -11111;
+    int size, capacity;
+
+    char* content = 0;
+
+    void* ffi_handle = 0;
+    void** ptr_to_ffi_handle = &ffi_handle;
+
+    printf("\n=========================================================================\n\n");
+    printf("Initial value of result: %d\n\n", res);
+
+    printf("\nInvoking...\n");
+    res = init_logging();
+    printf("Invoked and got result: %d\n\n", res);
+
+    printf("\nCreating unregistered client...\n");
+    res = create_unregistered_client(ptr_to_ffi_handle);
+    printf("Invoked and got result: %d\n\n", res);
+
+    printf("\nGetting stats for GETs...\n");
+    res = client_issued_gets(ffi_handle);
+    printf("Invoked and got stats for GETs: %d\n\n", res);
+
+    printf("\nInvoking execute with invalid JSON...\n");
+    res = execute("This is an invalid JSON.", ffi_handle);
+    printf("Invoked and got result: %d\n\n", res);
+
+    printf("\nInvoking execute with valid JSON (Getting service \"www\" for long-name \"spandan\")"
+            "...\n");
+    content = execute_for_content("{\"module\": \"dns\",\"action\": \"get-home-dir\", \"data\": "
+                                  "{\"long_name\": \"spandan\", \"service_name\": \"www\"}}",
+                                  &size, &capacity, &res, ffi_handle);
+    printf("Invoked and got result: %d", res);
+    if (!res) {
+        printf("\nContent: %s\n\n", content);
+    } else {
+        printf("\n\n");
+    }
+
+    printf("\nGetting stats for GETs...\n");
+    res = client_issued_gets(ffi_handle);
+    printf("Invoked and got stats for GETs: %d\n\n", res);
+
+    printf("=========================================================================\n\n");
+    return 0;
+}
